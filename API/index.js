ce741d8f62896b5878dca0eb6dbc1db99cca0d93
LeozinLSB <leonardosilvab007@gmail.com>
2026-04-21 18:50:06 -0300

    correcao: adicionada variavel de ambiente para mongoDB

--- API/index.js
+++ API/index.js
@@ -18,7 +18,7 @@ app.listen(PORT, () => {
 })
 // Obtendo os parametros passados pela linha de comando
 var userArgs = process.argv.slice(2);
-var mongoURL = userArgs[0];
+var mongoURL = process.env.MONGO_URI || userArgs[0];
 //Configurando a conexao com o Banco de Dados
 var mongoose = require('mongoose');
 mongoose.connect(mongoURL);
