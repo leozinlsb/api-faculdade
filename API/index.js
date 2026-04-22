013f38a46e4216e5c6b8f970280528f6f3583bdc
LeozinLSB <leonardosilvab007@gmail.com>
2026-04-22 19:25:47 -0300

    corrige bug do cors travando a porta + comments para deixar o codigo mais legivel

--- API/index.js
+++ API/index.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use((req, res, next) => {app.use(cors());});
+
+// Apenas isso, direto e reto! O buraco negro foi fechado.
+app.use(cors());
 app.use(express.json());
+
 const routes = require('./routes/routes');
 app.use('/api', routes);
+
 // Obtendo os parametros passados pela linha de comando
 var userArgs = process.argv.slice(2);
 var mongoURL = process.env.MONGO_URI || userArgs[0];
+
 //Configurando a conexao com o Banco de Dados
 var mongoose = require('mongoose');
 mongoose.connect(mongoURL);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', (error) => {
- console.log(error)
+  console.log(error)
 })
 db.once('connected', () => {
- console.log('Database Connected');
+  console.log('Database Connected');
 })
+
 const PORT = process.env.PORT || 3000;
 // O segredo do Render está em adicionar o "0.0.0.0"
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Servidor rodando liso na porta ${PORT}`);
+  console.log(`Servidor rodando liso na porta ${PORT}`);
 });
