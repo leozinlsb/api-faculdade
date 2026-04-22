45539d1aaa2db7056eafb1fc45f245fa1e9812ff
LeozinLSB <leonardosilvab007@gmail.com>
2026-04-21 22:42:56 -0300

    corrige as portas para o deploy do render v2

--- API/index.js
+++ API/index.js
@@ -3,7 +3,13 @@ const app = express();
 const cors = require('cors');
 app.use((req, res, next) => {app.use(cors());});
 app.use(express.json());
+// O Render injeta a porta na variável process.env.PORT. 
+// Se ela não existir (no seu PC local), ele usa a 3000.
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando e ouvindo na porta ${PORT}`);
+});
 const routes = require('./routes/routes');
 app.use('/api', routes);
 app.listen(PORT, () => {
@@ -23,10 +29,3 @@ db.on('error', (error) => {
 db.once('connected', () => {
  console.log('Database Connected');
 })
-// O Render injeta a porta na variável process.env.PORT. 
-// Se ela não existir (no seu PC local), ele usa a 3000.
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando e ouvindo na porta ${PORT}`);
-});
